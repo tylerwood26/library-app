b04ba43a855a2d5926b4620e448b21921d6eac04
tylerwood26 <24847346+tylerwood26@users.noreply.github.com>
2025-06-06 17:08:14 -0400

    Display books on page using dom

--- app.js
+++ app.js
@@ -8,14 +8,43 @@ function Book(id, title, author, pages, read) {
     this.read = read;
 }
 
+// next 2 lines are temp test 
+const testBook = new Book(2, 'Test', 'Tyler', 302, 'not read');
+myLibrary.push(testBook);
+
 function addBookToLibrary(title, author, pages, read) {
     let id = self.crypto.randomUUID();
     const newBook = new Book(id, title, author, pages, read)
     myLibrary.push(newBook);
 }
 
-function displayBooks() {
-    // loop through array
+function displayBooks(book) {
+    const htmlContainerDiv = document.querySelector(".container");
+    const createNewCard = document.createElement("div");
+
+    const titleH1 = document.createElement("h1");
+    titleH1.textContent = book.title;
     
-    // display book to page
+    const authorH2 = document.createElement("h2");
+    authorH2.textContent = book.author;
+    
+    const pagesH3 = document.createElement("h3");
+    pagesH3.textContent = book.pages;
+    
+    const readH3 = document.createElement("h3");
+    readH3.textContent = book.read;
+    
+    const idH4 = document.createElement("h4");
+    idH4.textContent = book.id;
+    
+    createNewCard.appendChild(titleH1);
+    createNewCard.appendChild(authorH2);
+    createNewCard.appendChild(pagesH3);
+    createNewCard.appendChild(readH3);
+    createNewCard.appendChild(idH4);
+
+    // append card div to container
+    htmlContainerDiv.appendChild(createNewCard);
 }
+
+myLibrary.forEach(displayBooks);
